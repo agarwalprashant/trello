cad291e42ad5fd37174fb2b33e07fb570e3ca36f
Prashant <agarwalprashant355@gmail.com>
2016-10-06 01:04:02 +0530

    second commit with boards button functionality

--- app.js
+++ app.js
@@ -6,16 +6,34 @@
 
 	function MainController($scope){
 		$scope.message = "trello";
-	
+		$scope.showDropList  = false;
+
+
+	$scope.showDropDownList = function(){
+		console.log("boards button clicked");
+		var array = [];
+		for(var i = 0;i<10;i++){
+			array.push(i);
+		}
+			// console.log(array);
+
+
+		$scope.dropListItems = array;
+		// console.log($scope.dropListItems);
+		$scope.showDropList = !$scope.showDropList;
+	}
+
+
+
 		}
 
 
-	app.directive("firstDirective",function(){
-		return {
-			restrict:"E",
-			scope:false,
-			templateUrl:"headerBar.html"
-		}
-	})
+	// app.directive("firstDirective",function(){
+	// 	return {
+	// 		restrict:"E",
+	// 		scope:false,
+	// 		templateUrl:"headerBar.html"
+	// 	}
+	// })
 
 }());
